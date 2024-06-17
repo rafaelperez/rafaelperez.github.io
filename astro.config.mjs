a12e76e76507c017cb7e1abf016eebfbf6542321
Rafael Silva <rafael@rafael.ai>
2024-06-17 00:00:56 -0400

    astro init

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://rafaelsilva.github.io',
+    // base: 'my-repo',
+});
